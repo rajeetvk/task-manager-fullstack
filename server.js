25fb14de9839919a9b3f95b0fa73e1dfefd6834b
Arjeet Singh <arjeet008.gzp@gmail.com>
2026-02-28 11:03:37 +0530

    Bind server to 0.0.0.0 for railway

--- server.js
+++ server.js
@@ -18,9 +18,16 @@ app.use("/api/tasks",taskroutes);
 
 
 
-const PORT = process.env.PORT || 5000;
+/*const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
+});*/
+
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server started on port ${PORT}`);
 });
 
