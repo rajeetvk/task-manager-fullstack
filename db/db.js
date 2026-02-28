bc7e4603febd17869185cbe8c581099f110286e1
Arjeet Singh <arjeet008.gzp@gmail.com>
2026-02-28 10:56:03 +0530

    Use MQSQL_PUBLIC_URL for railway

--- db/db.js
+++ db/db.js
@@ -1,12 +1,6 @@
 const mysql = require("mysql2");
 
-const db = mysql.createConnection({
-    host: process.env.MYSQLHOST,
-    user: process.env.MYSQLUSER,
-    password: process.env.MYSQLPASSWORD,
-    database: process.env.MYSQLDATABASE,
-    port: process.env.MYSQLPORT
-});
+const db = mysql.createConnection(process.env.MYSQL_PUBLIC_URL);
 
 db.connect((err) => {
     if (err) {
